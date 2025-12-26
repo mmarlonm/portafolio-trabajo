636d4af61affec692836e0184c39a0c41b5fa66a
c.marlgm <marlon93gm@gmail.com>
2025-12-26 15:12:42 -0600

    solucion deploy

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@ const repo = 'portafolio-trabajo';
 
 module.exports = {
   output: 'export',
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
   trailingSlash: true,
   images: {
     unoptimized: true,
