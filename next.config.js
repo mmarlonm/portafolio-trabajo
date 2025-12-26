736fe2d871454e7bffafc776e030e679fe603c03
c.marlgm <marlon93gm@gmail.com>
2025-12-26 15:08:33 -0600

    release

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const repo = 'portafolio-trabajo';
 module.exports = {
   output: 'export',
   basePath: `/${repo}`,
-  assetPrefix: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   trailingSlash: true,
   images: {
     unoptimized: true,
