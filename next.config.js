f74682ce15bde658d396a23f95b40011d04379e5
c.marlgm <marlon93gm@gmail.com>
2025-12-26 12:58:02 -0600

    release

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/portafolio-trabajo',
+  assetPrefix: '/portafolio-trabajo/',
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = nextConfig
