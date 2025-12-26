8aa17fddf2ffb89af33300d75c36b81ac2f61b94
c.marlgm <marlon93gm@gmail.com>
2025-12-26 16:10:06 -0600

    productivo coreccion

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: 'export',
   basePath: '/portafolio-trabajo',
-  assetPrefix: '/portafolio-trabajo/',
+  assetPrefix: '/portafolio-trabajo',
   images: {
     unoptimized: true
   }
